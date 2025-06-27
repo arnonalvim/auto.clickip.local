287fe7adc9ad17aa24fa36f171fd3f007c56ec5f
arnonalvim <arnon.bot777@gmail.com>
2025-06-27 05:27:18 -0400

    fix: deixando o código menos feio

--- app/controllers/routes/home.py
+++ app/controllers/routes/home.py
@@ -3,17 +3,18 @@ from flask_login import login_required, current_user
 
 home_bp = Blueprint('home_bp', __name__)
 
+
 @home_bp.route('/')
 def home():
     # Se o usuário não estiver logado, redireciona para login
     if not current_user.is_authenticated:
         return redirect(url_for('auth_bp.login'))
-    
     # Se estiver logado, mostra a home
     return render_template('home.html')
 
-# Ou você pode usar o decorator diretamente:
-@home_bp.route('/dashboard')  # Rota alternativa protegida
+
+# Usano uma rota alternativa para a home caso o fábio crie uma, eu não vou
+@home_bp.route('/')  # rota alternativa
 @login_required
 def dashboard():
     return render_template('home.html')
