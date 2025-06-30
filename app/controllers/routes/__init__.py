fc3c21b4f010c229673e42abfe7f3e84797b8918
arnonalvim <arnon.bot777@gmail.com>
2025-06-30 19:59:05 -0400

    style: limpando código

--- app/controllers/routes/__init__.py
+++ app/controllers/routes/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from .auth import auth_bp
 
-
+from .home import home_bp
 from .dmos.downstream_fec import downstream_fec_bp
 from .dmos.shutdown_gpon import shutdown_gpon_bp
 from .dmos.reset_onus_gpon import reset_onus_gpon_bp
 from .dmos.get_mpls_brief import get_mpls_brief_bp
 from .dmos.vlan_config import vlan_config_bp
 from .dmos.mpls_l2vpn_tunnel import mpls_l2vpn_tunnel_bp
-from .home import home_bp
 from .junos.interface_dhcp import int_dhcp_bp
 from .junos.bgp_manager import bgp_manager_bp
 from .junos.get_interface_ae0_config import int_conf_bp
@@ -20,7 +19,7 @@ from .junos.set_interface_ae0_unit_vlan import set_interface_ae0_unit_vlan_bp
 
 def register_blueprints(app: Flask):
     app.register_blueprint(auth_bp, url_prefix='/auth')
-
+    app.register_blueprint(home_bp)
     app.register_blueprint(downstream_fec_bp, url_prefix='/network')
     app.register_blueprint(shutdown_gpon_bp, url_prefix='/network')
     app.register_blueprint(reset_onus_gpon_bp, url_prefix='/network')
@@ -34,4 +33,3 @@ def register_blueprints(app: Flask):
     app.register_blueprint(int_static_route_bp, url_prefix='/network')
     app.register_blueprint(set_policy_whitelist_bp, url_prefix='/network')
     app.register_blueprint(set_interface_ae0_unit_vlan_bp, url_prefix='/network')
-    app.register_blueprint(home_bp)
