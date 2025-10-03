44a4e4fe0a70d21674c267afae8212829f26a4e3
Arnon Alvim <alvimarnon@gmail.com>
2025-10-02 21:38:19 -0400

    fix: mais portas em switches

--- app/controllers/forms/vlan_config.py
+++ app/controllers/forms/vlan_config.py
@@ -22,10 +22,10 @@ class Vlan_config_form(FlaskForm):
         render_kw={"placeholder": "Digite uma descrição para a VLAN"}
     )
 
-    eth_ports = [f"hundred-gigabit-ethernet-1/1/{i}" for i in range(1, 7)]
-    ten_gig_ports = [f"ten-gigabit-ethernet-1/1/{i}" for i in range(1, 9)]
-    twf_gig_ports = [f"twenty-five-g-ethernet-1/1/{i}" for i in range(1, 17)]
-    lags = [f"lag-{i}" for i in range(1, 9)]
+    eth_ports = [f"hundred-gigabit-ethernet-1/1/{i}" for i in range(1, 48)]
+    ten_gig_ports = [f"ten-gigabit-ethernet-1/1/{i}" for i in range(1, 48)]
+    twf_gig_ports = [f"twenty-five-g-ethernet-1/1/{i}" for i in range(1, 48)]
+    lags = [f"lag-{i}" for i in range(1, 20)]
 
     port_choices = eth_ports + ten_gig_ports + twf_gig_ports + lags
 
